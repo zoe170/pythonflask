63b0e48aa7f5ec9676d1c09639f4d608d35c8db8
Zoe <boninguezoe@gmail.com>
2026-02-11 16:23:42 +0100

    mise a jour

--- .history/app_20260211162141.py
+++ .history/app_20260211162141.py
@@ -1,3 +1,50 @@
+# from flask import Flask, render_template
+
+# app = Flask(__name__)
+# style = '<link rel="stylesheet" href="/static/style.css">'
+
+# @app.route('/')
+# def home():
+#     return """
+#     <h1>Bienvenue sur ma page d'accueil !</h1>
+#     <a href="/about">Aller vers la page À propos</a><br>
+#     <a href="/project">Aller vers la page Projet</a> <br>
+#     <a href="/map">Aller vers la carte</a>
+#     """
+
+# @app.route('/about')
+# def about():
+#     return """
+#     <h1>À propos</h1>
+#     <p>Je suis en train d'apprendre à développer des applications web avec Flask !</p>
+#     <a href="/">Retour à l'accueil</a>
+#     """
+
+# @app.route('/project')
+# def project():
+#     return """
+#     <h1>Page Projet</h1>
+#     <p>Bienvenue sur ma page projet !</p>
+#     <a href="/">Retour à l'accueil</a>
+#     """
+
+
+# @app.route('/map')
+# def map():
+#     return render_template("map.html")
+
+
+
+# @app.route('/photo')
+# def photo():
+#     return render_template("photo.html")
+
+
+
+# if __name__ == '__main__':
+#     app.run(debug=True, port=5001)
+
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -34,7 +81,7 @@ style = """
     a {
         text-decoration: none;
         color: white;
-        background-color: 424270;
+;
         padding: 10px 20px;
         border-radius: 6px;
         font-weight: bold;
@@ -43,7 +90,7 @@ style = """
     }
 
     a:hover {
-        background-color: 424270;
+        background-color: #36a420;
         transform: scale(1.05);
     }
 </style>
